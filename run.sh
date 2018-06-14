4461471136c2611737fe2675ffcb3b1cf64d6d0a
esrrhs <esrrhs@163.com>
2018-06-14 10:58:03 +0800

    add

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 docker stop xware
 docker rm xware
 
-dir=/home/xunlei
+dir=/mnt/usb/xunlei/
 mkdir -p $dir
 mkdir -p $dir/download
+rm $dir/download/ThunderDB/ -rf
 
 docker run --name xware --privileged --restart=always -v $dir/download:/project/download -d esrrhs/docker-thunder-xware
--- start.sh
+++ start.sh
@@ -2,11 +2,16 @@
 
 mount --bind /project/download/ /mnt/xunlei
 
+./portal
+
+sleep 60
+
 while true; do
     pid=`ps -ef|grep vod_httpserver|grep -v grep|awk '{print $2}'`
     if [ -z "$pid" ]; then
-        ./portal
+        exit
     else
         sleep 60
     fi
 done
+
