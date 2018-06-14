d08b6613a49fd6a7b68ae9e89e4b5ad3c025d961
esrrhs <esrrhs@163.com>
2018-06-14 11:34:23 +0800

    add

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ docker rm xware
 dir=/mnt/usb/xunlei/
 mkdir -p $dir
 mkdir -p $dir/download
-rm $dir/download/ThunderDB/ -rf
 
-docker run --name xware --privileged --restart=always -v $dir/download:/project/download -d esrrhs/docker-thunder-xware
+r1=$(( (RANDOM % 89) + 10 ))
+r2=$(( (RANDOM % 89) + 10 ))
+
+mac=`echo "00:00:00:00:"$r1":"$r2`
+echo $mac
+
+docker run --name xware --privileged --mac-address $mac --restart=always -v $dir/download:/project/download -d esrrhs/docker-thunder-xware
